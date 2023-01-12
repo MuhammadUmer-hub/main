a0484508a1060b1db5cd7e11c4d9a27edf1e762e
MuhammadUmer-hub <76170624+MuhammadUmer-hub@users.noreply.github.com>
2023-01-12 15:29:39 +0500

    Add files via upload

---
+++ Main/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+ public static < E > void printArray( E[] inputArray ) {
+	 for (int i=inputArray.length-1; i>=0; i--) {
+			System.out.println(inputArray[i]);
+		}
+		}
+ public static void main(String args[]) {
+ // Create arrays of Integer, Double and Character
+ Integer[] intArray = { 1, 2, 3, 4, 5 };
+ Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+ Character[] charArray = { 'h', 'a', 'm', 'z', 'a' };
+ System.out.println("Array integerArray contains:");
+ printArray(intArray); // pass an Integer array
+ System.out.println("\nArray doubleArray contains:");
+ printArray(doubleArray); // pass a Double array
+ System.out.println("\nArray characterArray contains:");
+ printArray(charArray); // pass a Character array
+ }
+}
